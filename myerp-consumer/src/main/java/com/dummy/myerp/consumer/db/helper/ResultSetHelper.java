a4cc858919e2de570fa8017a86943bc1de1241ad
nanoo <arnaudlaval33@gmail.com>
2020-03-18 17:07:10 +0100

    add new job report for gitlab-ci.yml

--- myerp-consumer/src/main/java/com/dummy/myerp/consumer/db/helper/ResultSetHelper.java
+++ myerp-consumer/src/main/java/com/dummy/myerp/consumer/db/helper/ResultSetHelper.java
@@ -36,7 +36,7 @@ public abstract class ResultSetHelper {
         Integer vRetour = null;
         int vInt = pRS.getInt(pColName);
         if (!pRS.wasNull()) {
-            vRetour = Integer.valueOf(vInt);
+            vRetour = vInt;
         }
         return vRetour;
     }
@@ -54,7 +54,7 @@ public abstract class ResultSetHelper {
         Long vRetour = null;
         Long vLong = pRS.getLong(pColName);
         if (!pRS.wasNull()) {
-            vRetour = Long.valueOf(vLong);
+            vRetour = vLong;
         }
         return vRetour;
     }
